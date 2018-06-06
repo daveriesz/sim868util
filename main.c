534daac6bee4fe624ba23020cd313ee5157b32a8
dpr <dave@iesz.net>
2018-06-06 16:38:59 -0700

    more stuff

--- main.c
+++ main.c
@@ -7,36 +7,59 @@
  *                                                                             *
  * optsions:                                                                   *
  *     -b <data_rate>             - data/baud rate                             *
- *     -w                         - wait for response                          *
- *     -t                         - wait timeout (secs.) (0 for indefinite)    *
- *     -1 <tx_string> <rx_string> - command to send and expected response      *
- *     -2 ... -n                  - same as -1, executed in numerical order    *
- *                                                                             *
+ *     -t <seconds>               - wait timeout (secs.) (0 for indefinite)    *
+ *     -c <tx_string> <rx_string> - command to send and expected response      *
+ *                                  Multiple commands can be specified and are *
+ *                                  executed in the order they are listed.     *
  *                                                                             *
  ******************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
 
-
-const char *usage()
-{
-  const char *usage_str[] = {
-    "sim868init",
-    "",
-    "usage: sim868init <serial_device> [options]",
-    "",
-    "optsions:",
-    "    -b <data_rate>             - data/baud rate",
-    "    -w                         - wait for response",
-    "    -t                         - wait timeout (secs.) (0 for indefinite)",
-    "    -1 <tx_string> <rx_string> - command to send and expected response",
-    "    -2 ... -n                  - same as -1, executed in numerical order",
-  };
-}
-
+#include "main.h"
+#include "args_local.h"
+#include "serial_local.h"
 
 int main(int argc, char **argv)
 {
+  struct run_parameters params = { NULL, -1, -1, NULL, -1, 0 };
+  parse_args(argc, argv, &params);
 
+  if(1)
+  {
+    struct tx_rx_pair *trp;
+    printf("serial_device = >>%s<<\n", params.tty);
+    printf("data_rate     = %d\n", params.data_rate);
+    printf("timeout       = %d\n", params.timeout);
+    for(trp = params.command_pairs ; trp ; trp=trp->next)
+    {
+      printf("cmd tx = >>%s<<\n    rx = >>%s<<\n", trp->tx, trp->rx);
+    }
+  }
+  if(params.tty == NULL)
+  {
+    fprintf(stderr, "Serial device not set.\n");
+    exit(1);
+  }
+  
+  return run_serial_commands(&params);
+  
+}
+
+void usage()
+{
+  const char *usage_str =
+    "usage: sim868init <serial_device> [options]\n"
+    "\n"
+    "optsions:\n"
+    "    -b <data_rate>             - data/baud rate\n"
+    "    -t <seconds>               - wait timeout (0 for indefinite)\n"
+    "    -c <tx_string> <rx_string> - command to send and expected response\n"
+    "                                 Multiple commands can be specified and are\n"
+    "                                 executed in the order they are listed.\n"
+    ;
+  fprintf(stderr, "%s", usage_str);
+  exit(1);
 }
 
--- serial.c
+++ serial.c
@@ -1,6 +1,100 @@
-int ix = 0;
 
-int serial_init()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "main.h"
+#include "serial_local.h"
+
+static int set_baud_rate(int data_rate)
 {
-
+  unsigned int ti_rate = 0;
+  switch(data_rate)
+  {
+    case      50: ti_rate =      B50; break;
+    case      75: ti_rate =      B75; break;
+    case     110: ti_rate =     B110; break;
+    case     134: ti_rate =     B134; break;
+    case     150: ti_rate =     B150; break;
+    case     200: ti_rate =     B200; break;
+    case     300: ti_rate =     B300; break;
+    case     600: ti_rate =     B600; break;
+    case    1200: ti_rate =    B1200; break;
+    case    1800: ti_rate =    B1800; break;
+    case    2400: ti_rate =    B2400; break;
+    case    4800: ti_rate =    B4800; break;
+    case    9600: ti_rate =    B9600; break;
+    case   19200: ti_rate =   B19200; break;
+    case   38400: ti_rate =   B38400; break;
+    case   57600: ti_rate =   B57600; break;
+    case  115200: ti_rate =  B115200; break;
+    case  230400: ti_rate =  B230400; break;
+    case  460800: ti_rate =  B460800; break;
+    case  500000: ti_rate =  B500000; break;
+    case  576000: ti_rate =  B576000; break;
+    case  921600: ti_rate =  B921600; break;
+    case 1000000: ti_rate = B1000000; break;
+    case 1152000: ti_rate = B1152000; break;
+    case 1500000: ti_rate = B1500000; break;
+    case 2000000: ti_rate = B2000000; break;
+    case 2500000: ti_rate = B2500000; break;
+    case 3000000: ti_rate = B3000000; break;
+    case 3500000: ti_rate = B3500000; break;
+    case 4000000: ti_rate = B4000000; break;
+    default:
+      fprintf(stderr, "unsupported rate: %d\n", data_rate);
+      fprintf(stderr, "valid rates are: 50, 75, 110, 134, 150, 200, 300, 600, 1200, 1800 2400,\n");
+      fprintf(stderr, "                 4800, 9600, 19200, 38400, 57600, 115200, 230400, 460800,\n");
+      fprintf(stderr, "                 500000, 576000, 921600, 1000000, 1152000, 1500000,\n");
+      fprintf(stderr, "                 2000000, 2500000, 3000000, 3500000, and 4000000.\n");
+      exit(1);
+  }
+  return ti_rate;
 }
+
+static int open_serial_device(struct run_parameters *params)
+{
+  params->fd = open(params->tty, O_RDWR|O_NOCTTY|O_NDELAY);
+  if(params->fd < 0)
+  {
+    fprintf(stderr, "failed to open device >>%s<<: %s\n", params->tty, strerror(errno));
+    exit(1);
+  }
+  else
+  {
+    fcntl(params->fd, F_SETFL, 0); /* blocking */
+//    fcntl(params->fd, SETFL, FNDELAY); /* non-blocking */
+  }
+  return params->fd;
+}
+
+static int configure_serial_device(struct run_parameters *params)
+{
+  struct termios options;
+  tcgetattr(params->fd, &options);
+  cfsetispeed(&options, params->baud_rate);
+  cfsetospeed(&options, params->baud_rate);
+  options.c_cflag |= (CLOCAL | CREAD);
+  tcsetattr(params->fd, TCSANOW, &options);
+  
+  return 0;
+}
+
+
+int run_serial_commands(struct run_parameters *params)
+{
+  int fd;
+  unsigned int baud_rate;
+  params->baud_rate = set_baud_rate(params->data_rate);
+  
+  open_serial_device(params);
+  configure_serial_device(params);
+  
+
+  return 0;
+}
+
